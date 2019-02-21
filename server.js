8459bcc47aaf0ca2a673df68cd7af04c88bef949
Daniel Ors <do7811@gmail.com>
2019-02-21 00:53:53 -0500

    validated api

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+/* eslint-disable quotes */
 'use strict';
-
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
 const app = express();
-
 app.use(morgan('dev'));
-
-app.use((req, res) => {
-  res.send('Hello, world from nodemon!');
-});
-
 const PORT = 8000;
+
+console.log(process.env.API_TOKEN);
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychich`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req,res) {
+  res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req,res) {
+  res.send('Hello, Pokemon!');
+}
+
+app.get('/pokemon', handleGetPokemon);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
